9fc3903a0eaab66f5c2d14da0b926027d7936701
MekaKuu27 <shanigin2007@gmail.com>
2021-07-04 03:16:34 +0300

    Updated file

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from PIL import Image, ImageDraw
-#Фон для заднего фона
-bgcolor=Image.new('RGB', (1000, 1000), (96, 141, 209))
+from PIL import Image, ImageDraw, ImageFont #Модуль ImageDraw -> отрисовка, # ImageFont -> для параметра шрифта и отрисовки текста.
+bgcolor=Image.new('RGB', (1000, 1000), (96, 141, 209)) #Фон для заднего фона
 draw = ImageDraw.Draw(bgcolor)
-draw.ellipse((1, 1, 300, 300), fill='black', outline=(0, 0, 0)) #отрисовка эллипса x0, y0 -> от края окна; x1, y1 -> размеры самого эллипса
-#draw.rectangle((10,450,300,150), fill = 'pink', outline=(0,0,0))
-#bgcolor.save('draw-ellipse-rectangle-line.jpg', quality=95)
+Font = ImageFont.truetype('11874.ttf', size=32) # Параметры шрифта для отрисовки текста
+draw.text((350, 20), 'Отрисовка фигур с помощью PIL(Pillow)', font=Font, fill='black') # Отрисовка текста
+draw.ellipse((1, 1, 300, 300), fill='red', outline=(0, 0, 0), width=2) #отрисовка эллипса x0, y0 -> от края окна; x1, y1 -> размеры самого эллипса
+draw.rectangle((20,310,450,550), fill = 'yellow', outline=(0,0,0), width=2) # отрисовка прямогульника
+#bgcolor.save('draw-ellipse-rectangle-line.jpg', quality=95) # создание картинки с нарисованными фигурами
 bgcolor.show()
